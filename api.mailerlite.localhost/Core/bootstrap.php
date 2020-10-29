23267ac0d84e35598be6e495374ade154b7e6309
unknown <samuel.moy@mac.com>
2020-10-29 11:22:16 +0100

    Some lines weren't PSR-12 enough

--- api.mailerlite.localhost/Core/bootstrap.php
+++ api.mailerlite.localhost/Core/bootstrap.php
@@ -11,7 +11,7 @@ $app = new App();
 $request = $app->container->get('Request');
 
 //Check if the request is valid
-if (!$request->isValid()){
+if (!$request->isValid()) {
 
     $app->sendResponse(
         \Symfony\Component\HttpFoundation\Response::HTTP_BAD_REQUEST,
--- api.mailerlite.localhost/Core/routes.php
+++ api.mailerlite.localhost/Core/routes.php
@@ -3,31 +3,31 @@
 /** @var FastRoute\RouteCollector $r */
 
 //Subscriber routes
-$r->get( '/subscribers', 'SubscribersController@get');
+$r->get('/subscribers', 'SubscribersController@get');
 
-$r->get( '/subscribers/{subscriber:\d+}', 'SubscribersController@get');
+$r->get('/subscribers/{subscriber:\d+}', 'SubscribersController@get');
 
-$r->post( '/subscribers', 'SubscribersController@post');
+$r->post('/subscribers', 'SubscribersController@post');
 
 $r->put('/subscribers/{subscriber:\d+}', 'SubscribersController@put');
 
 $r->delete('/subscribers/{subscriber:\d+}', 'SubscribersController@delete');
 
 //Fields routes
-$r->get( '/fields', 'FieldsController@get');
+$r->get('/fields', 'FieldsController@get');
 
-$r->get( '/fields/{field:\d+}', 'FieldsController@get');
+$r->get('/fields/{field:\d+}', 'FieldsController@get');
 
-$r->post( '/fields', 'FieldsController@post');
+$r->post('/fields', 'FieldsController@post');
 
 $r->put('/fields/{field:\d+}', 'FieldsController@put');
 
 $r->delete('/fields/{field:\d+}', 'FieldsController@delete');
 
 //SubscriberFields routes
-$r->get( '/subscribers/{subscriber:\d+}/fields', 'SubscribersFieldsController@get');
+$r->get('/subscribers/{subscriber:\d+}/fields', 'SubscribersFieldsController@get');
 
-$r->post( '/subscribers/{subscriber:\d+}/fields', 'SubscribersFieldsController@post');
+$r->post('/subscribers/{subscriber:\d+}/fields', 'SubscribersFieldsController@post');
 
 $r->put('/subscribers/{subscriber:\d+}/fields/{id:\d+}', 'SubscribersFieldsController@put');
 
